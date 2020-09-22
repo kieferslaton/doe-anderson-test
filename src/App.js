331c87712d9b8dde06dbb1fcc14e14bcf1b277cf
Kiefer Slaton <kieferslaton@Kiefers-MacBook-Pro.local>
2020-09-22 09:11:13 -0500

    Axios fetch then JSON stringify just to see what data we're working with

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import axios from 'axios'
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
+  const getNewUser = () => {
+    axios.get('https://www.randomuser.me/api').then(res => setUser(res.data.results)).catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getNewUser()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {user ? JSON.stringify(user) : 'Error fetching data'}
     </div>
   );
 }
